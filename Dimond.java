5608daacb8994b86612eaade522da9d5811d2290
Shaik Bunglow Samreen <shaiksamreensam123@gmail.com>
2024-02-28 21:21:45 +0530

    main

---
+++ Dimond.java
@@ -0,0 +1,22 @@
+public class Dimond {
+    public static void main(String[] args) {
+        for(int j=1;j<=5;j++){
+            for(int k=5;k>=j;k--){
+                System.out.print(" ");
+            }
+            for(int i=1;i<=2*j-1;i++){
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+        for(int j=1;j<=5;j++){
+            for(int k=1;k<=j;k++){
+                System.out.print(" ");
+            }
+            for(int i=9;i>=2*j-1;i--){
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+    }
+}
